b5bb8627f201bd68f53d063e5ad3ef476bbdebef
Alejandro Wu <alejandrowu189@gmail.com>
2021-11-05 21:35:03 -0400

    Version

---
+++ SecuenciaFibonacci2/SecuenciaFibonacci2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SecuenciaFibonacci2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0, Azul;
+            Console.WriteLine("Secuencia Fibonacci");
+            Console.WriteLine("Escriba el digito para la cantidad de sucesion:");
+            
+            Azul = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int i = 2; i < Azul; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                if (c % 2 == 0)
+                {
+                    Console.Write(c + " Par,");
+                }
+                else
+                {
+                    Console.Write(c + " Impar,");
+                }
+
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine("No es primo,");
+                }
+                else
+                {
+                    Console.WriteLine("primo,");
+                }
+
+            }
+               
+        }
+    }
+}
